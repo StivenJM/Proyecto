36afc2d970d956897db3c96613e3fc232c2176b4
StivenJM <stevenmoposita456@gmail.com>
2023-02-01 22:50:17 -0500

    Añadiendo archivos a modelo

---
+++ src/modelo/Votante.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Votante extends Persona{
+    
+    public Votante(String nombres, String apellidos, int[] fechaNacimiento, String numeroCedula) {
+        super(nombres, apellidos, fechaNacimiento, numeroCedula);
+    }
+    
+    public void votarPorBinomio(Binomio binomio){
+        binomio.agregarVotoAFavor();
+    }
+    
+}
